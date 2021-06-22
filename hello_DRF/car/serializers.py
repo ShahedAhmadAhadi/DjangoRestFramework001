69619aa846bc4c541d7d95fca815727d0c1a48f5
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-22 21:56:06 +0430

    new app created the car serializers moved to new app 'car'

---
+++ hello_DRF/car/serializers.py
@@ -0,0 +1,18 @@
+from re import search
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from .models import Car
+
+class CarSerializer(serializers.Serializer):
+    no_plate = serializers.IntegerField()
+    model = serializers.CharField(max_length=10)
+    color = serializers.CharField(max_length=12)
+
+    def create(self, validated_data):
+        return Car.create(validated_data)
+
+    def update(self, instance, validated_data):
+        instance.model = validated_data.get('model', instance.model)
+        instance.color = validated_data.get('color', instance.color)
+        instance.save()
+        return instance
--- hello_DRF/hello_DRF/begin/serializers.py
+++ hello_DRF/hello_DRF/begin/serializers.py
@@ -1,7 +1,7 @@
 from re import search
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from models import Car
+from .models import Car
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -14,18 +14,3 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
-
-
-class CarSerializer(serializers.Serializer):
-    no_plate = serializers.IntegerField()
-    model = serializers.CharField(max_length=10)
-    color = serializers.CharField(max_length=12)
-
-    def create(self, validated_data):
-        return Car.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.model = validated_data.get('model', instance.model)
-        instance.color = validated_data.get('color', instance.color)
-        instance.save()
-        return instance
