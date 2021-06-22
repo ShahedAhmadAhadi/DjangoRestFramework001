653a7ce72175042d70e1897baaafb028c6b15195
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-22 21:43:23 +0430

    car model created to be tested by rest_framework.serializers.Serializer and rest_framework.serializers.ModelSerializer, .Serializers created

--- hello_DRF/hello_DRF/begin/models.py
+++ hello_DRF/hello_DRF/begin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Car(models.Model):
+    no_plate = models.IntegerField(primary_key=True, max_length=6, blank=False)
+    model = models.CharField(max_length=10)
+    color = models.CharField(max_length=12)
+
