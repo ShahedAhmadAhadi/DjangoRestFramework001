3298508557f1318dc0be3e4d3ea70dcd3b5bee90
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-23 15:29:29 +0430

    Post to update the car is succefully executed and testted

--- hello_DRF/car/views.py
+++ hello_DRF/car/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 from rest_framework import serializers
 from rest_framework.parsers import JSONParser
+from rest_framework.utils import json
 from .models import Person, Car
 from .serializers import PersonSerializer, CarSerializer
 
@@ -15,3 +16,12 @@ def car_list(request):
         car = Car.objects.all()
         serializer = CarSerializer(car, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = CarSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
