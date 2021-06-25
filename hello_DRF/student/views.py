f6bf05db78b0f0be8768cc6f6ec65ac19fec0591
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-25 17:25:52 +0430

    In student app for Student model the view is created with method -> GET, POST

--- hello_DRF/student/views.py
+++ hello_DRF/student/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from .serializers import StudentSerializer
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Student
 
 
 # Create your views here.
 
 
-@api_view
+@api_view(['GET', 'POST'])
 def student_list(request):
-    pass
+    if request.method == "GET":
+        student = Student.objects.all()
+        serializer = StudentSerializer(student, manay=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
