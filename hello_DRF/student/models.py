16e672ed769ba5016ee3d2092e595706aa8632e1
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-25 17:13:59 +0430

    new model in student app by name of Student and registered in admin panel

--- hello_DRF/student/models.py
+++ hello_DRF/student/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=18)
+    father_name = models.CharField(max_length=18)
+    age = models.IntegerField()
+
