ec0c36ee543e77938c43f5b04f5e5f3ffb20229b
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-25 17:19:37 +0430

    serializer.py file created and modelSerializer used for 'car model' and views file in student app updated

--- hello_DRF/student/views.py
+++ hello_DRF/student/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import StudentSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+
+
+@api_view
+def student_list(request):
+    pass
