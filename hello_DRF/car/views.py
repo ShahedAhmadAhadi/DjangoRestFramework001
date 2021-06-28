714d0aff2271886da92857402ffb86216ad50c27
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-28 15:43:01 +0430

    userViewSet and CarViewSet has been updated and urls added and routers added and binding for the viewsets done!

--- hello_DRF/car/views.py
+++ hello_DRF/car/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 from django.http import JsonResponse, HttpResponse
 from rest_framework import serializers
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import action, api_view, permission_classes, renderer_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.utils import json
 from .models import Person, Car
@@ -13,53 +13,75 @@ from .serializers import CarSerializer, PersonSerializer, UserSerializer
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework.reverse import reverse
-from rest_framework import renderers
+from rest_framework import renderers, viewsets
 from rest_framework.response import Response
 
 # Create your views here.
 
 
-class CarList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+# class CarList(mixins.ListModelMixin,
+#                   mixins.CreateModelMixin,
+#                   generics.GenericAPIView):
+#     queryset = Car.objects.all()
+#     serializer_class = CarSerializer
+#     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+
+# @csrf_exempt
+# def car_details(request, pk):
+#     try:
+#         car = Car.objects.get(no_plate=pk)
+#     except Car.DoesNotExist:
+#         return JsonResponse(status=404)
+
+#     if request.method == "GET":
+#         serializer = CarSerializer(car)
+#         return JsonResponse(serializer.data)
+
+#     elif request.method == "PUT":
+#         data = JSONParser().parse(request)
+#         serializer = CarSerializer(car, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=200)
+#         return JsonResponse(serializer.errors, status=400)
+
+#     elif request.method == "DELETE":
+#         car.delete()
+#         return JsonResponse(status=204)
+
+
+# class CarHighlight(generics.GenericAPIView):
+#     queryset= Car.objects.all()
+#     renderer_classes = [renderers.StaticHTMLRenderer]
+
+#     def get(self, request, *args, **kwargs):
+#         car = self.get_object()
+#         return Response(car.highlighted)
+
+
+class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        car = self.get_object()
+        return Response(car.highlighted)
+
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-
-@csrf_exempt
-def car_details(request, pk):
-    try:
-        car = Car.objects.get(no_plate=pk)
-    except Car.DoesNotExist:
-        return JsonResponse(status=404)
-
-    if request.method == "GET":
-        serializer = CarSerializer(car)
-        return JsonResponse(serializer.data)
-
-    elif request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = CarSerializer(car, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=200)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == "DELETE":
-        car.delete()
-        return JsonResponse(status=204)
-
+        return serializer.save(owner=self.request.user)
 
 @csrf_exempt
 def person_list(request):
@@ -119,10 +141,3 @@ def api_root(request, format=None):
     })
 
 
-class CarHighlight(generics.GenericAPIView):
-    queryset= Car.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
-        car = self.get_object()
-        return Response(car.highlighted)
--- hello_DRF/hello_DRF/urls.py
+++ hello_DRF/hello_DRF/urls.py
@@ -19,25 +19,57 @@ from rest_framework import routers
 from .begin import views
 from car import views as carView
 from student import views as studentViews
+from rest_framework import renderers
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'cars', carView.CarViewSet)
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('car/', carView.CarList.as_view(), name='car-list'),
-    path('car/<int:pk>', carView.car_details, name='car-detail'),
-    path('person/', carView.person_list),
-    path('person/<int:pk>', carView.person_details),
-    path('student/', studentViews.student_list),
-    path('student/<int:pk>/', studentViews.student_detail),
-    path('students/', studentViews.StudentName.as_view()),
-    path('users/', carView.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', carView.UserDetail.as_view(), name='user-detail'),
+car_list = carView.CarViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+car_detail = carView.CarViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+car_highlight = carView.CarViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list =views.UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = views.UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include(router.urls)),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+#     path('car/', carView.CarList.as_view(), name='car-list'),
+#     path('car/<int:pk>', carView.car_details, name='car-detail'),
+#     path('person/', carView.person_list),
+#     path('person/<int:pk>', carView.person_details),
+#     path('student/', studentViews.student_list),
+#     path('student/<int:pk>/', studentViews.student_detail),
+#     path('students/', studentViews.StudentName.as_view()),
+#     path('users/', carView.UserList.as_view(), name='user-list'),
+#     path('users/<int:pk>/', carView.UserDetail.as_view(), name='user-detail'),
+#     path('', carView.api_root),
+#     path('car/<int:pk>/highlight', carView.CarHighlight.as_view(), name='car-highlight'),
+
+# ]
+
+urlpatterns = format_suffix_patterns([
     path('', carView.api_root),
-    path('car/<int:pk>/highlight', carView.CarHighlight.as_view(), name='car-highlight'),
-
-]
+    path('cars/', car_list, name='car-list'),
+    path('cars/<int:pk>/', car_detail, name='car-detail'),
+    path('cars/<int:pk>/highlight/', car_highlight, name='car-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
+    path('', include(router.urls)),
+])
