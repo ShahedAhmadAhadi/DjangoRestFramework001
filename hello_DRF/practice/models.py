1fedd4cd33435b501e9962be668beff665467469
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-29 20:44:11 +0430

    new models added by name example to play around with it

--- hello_DRF/practice/models.py
+++ hello_DRF/practice/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.query_utils import select_related_descend
 
 # Create your models here.
+
+LANGUAGES_AVAILABLE = ['English', 'Espanish', 'Arabic', 'French', 'Persian']
+
+class ExampleModel(models.Model):
+    name = models.CharField(max_length=20)
+    language = models.CharField(choices=LANGUAGES_AVAILABLE, max_length=50)
