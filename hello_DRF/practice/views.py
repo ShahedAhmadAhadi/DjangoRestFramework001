607a2f49d0ca09d780a7a3c823c41729112503d5
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-29 23:52:41 +0430

    all request methods tested and written a function that writes all methods of request in a file

--- hello_DRF/practice/views.py
+++ hello_DRF/practice/views.py
@@ -1,3 +1,4 @@
+import re
 from django.core.checks.messages import Error
 from django.http.response import JsonResponse
 from django.shortcuts import render
@@ -17,39 +18,42 @@ from rest_framework.decorators import api_view
 def fileWriting(name, encoding, data, path = ""):
     try:
         with open(f'{path}{name}.txt', 'w', encoding=encoding) as file:
-            file.write(f"data : {data.data}")
-            file.write(f"user : {data.user}")
-            file.write(f"query_parmas : {data.query_params}")
-            file.write(f"parsers : {data.parsers}")
-            file.write(f"accepted_renderer : {data.accepted_renderer}")
-            file.write(f"accepted_media_type : {data.accepted_media_type}")
-            file.write(f"auth : {data.auth}")
-            file.write(f"authenticators : {data.authenticators}")
-            file.write(f"method : {data.method}")
-            file.write(f"content_type : {data.content_type}")
-            file.write(f"stream : {data.stream}")
-            file.write(f"META : {data.META}")
+            file.write(f"data : {data.data}\n")
+            file.write(f"user : {data.user}\n")
+            file.write(f"query_parmas : {data.query_params}\n")
+            file.write(f"parsers : {data.parsers}\n")
+            file.write(f"accepted_renderer : {data.accepted_renderer}\n")
+            file.write(f"accepted_media_type : {data.accepted_media_type}\n")
+            file.write(f"auth : {data.auth}\n")
+            file.write(f"authenticators : {data.authenticators}\n")
+            file.write(f"method : {data.method}\n")
+            file.write(f"content_type : {data.content_type}\n")
+            file.write(f"stream : {data.stream}\n")
+            file.write(f"META : {data.META}\n")
     finally:
         file.close()
+        print(data)
+        return data
 
 
 @api_view(['GET', 'POST'])
 def example(request):
 
     if request.method == 'GET':
-        fileWriting('GETRequestFile', 'utf-8', request)
+        request =  fileWriting('GETRequestFile', 'utf-8', request)
         example = ExampleModel.objects.all()
         serializer = ExampleSerializer(example, many=True)
         return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe=False)
 
     if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ExampleSerializer(data=data)
-
+        req = fileWriting('POSTRequestFile', 'utf-8', request)
+        # data = JSONParser().parse(request)
+        serializer = ExampleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response('req')
 
 
 def log(request):
