1df5c2d3526e299a3bb7fbcfae528bff9578b0b9
Shahed Ahmad <shaheda.a16@gmail.com>
2021-06-29 21:16:52 +0430

    example view and serailzier created

--- hello_DRF/practice/serailizers.py
+++ hello_DRF/practice/serailizers.py
@@ -1,9 +1,15 @@
 from django.db.models import fields
 from rest_framework import serializers
-from .models import ExampleModel
+from .models import ExampleModel, RequestLog
 
 
 class ExampleSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExampleModel
         fields = ['id', 'name', 'language']
+
+
+class RequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RequestLog
+        fields = ['id', 'request_by', 'request_for', 'request_log']
--- hello_DRF/practice/views.py
+++ hello_DRF/practice/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from rest_framework import request
+from rest_framework import serializers
+from .serailizers import ExampleSerializer, RequestSerializer
+from rest_framework.response import Response
 from .models import ExampleModel, RequestLog
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
+
 def example(request):
-    
+    if request.method == 'GET':
+        example = ExampleModel.objects.all()
+        serializer = ExampleSerializer(example, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ExampleSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def log(request):
+    pass
