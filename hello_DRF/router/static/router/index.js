9dbb415ec51fb682c475776941468f0c0f42cfed
Shahed Ahmad <shaheda.a16@gmail.com>
2021-07-04 23:56:48 +0430

    new model to add data from the randomuser api and givin by a click of template in there and back post the data and tested getting the data

--- hello_DRF/router/static/router/index.js
+++ hello_DRF/router/static/router/index.js
@@ -1,13 +1,29 @@
-async function add(){
-    let response = await fetch(
-        'https://randomuser.me/api/'
-        )
+async function add() {
+    let response = await fetch("https://randomuser.me/api/?results=10");
+
+    let data = await response.json();
+
+    console.log(data);
+    post(data.results)
     
-    let data = await response.json()
-    
-    console.log(data)
+}
+
+async function post(data) {
+    data.map(
+    item =>
+        fetch("http://localhost:8000/router/", {
+            headers: { "Content-type": "application/json" },
+            method: "POST",
+            body: JSON.stringify({
+                name: item.name.first + item.name.last,
+                gender: item.gender,
+                email: item.email,
+                phone: item.phone,
+            }),
+        })
+);
 }
 
 
-let btn = document.getElementById('add-btn')
-btn.addEventListener('click', add)
+let btn = document.getElementById("add-btn");
+btn.addEventListener("click", add);
--- hello_DRF/router/views.py
+++ hello_DRF/router/views.py
@@ -1,9 +1,14 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.translation import check_for_language
+from rest_framework.decorators import api_view
+from .models import Employee
 
 # Create your views here.
 
-
+@api_view(['GET', 'POST'])
 def create_records(request):
+    if request.method == "POST":
+        pass
     return render(request, './router/index.html')
