bb19817336e055d58d1e61962c15e20df9164f15
Shahed Ahmad <shaheda.a16@gmail.com>
2021-07-05 01:30:35 +0430

    refactor

--- hello_DRF/router/static/router/index.js
+++ hello_DRF/router/static/router/index.js
@@ -8,19 +8,18 @@ async function add() {
     
 }
 
+const csrftoken = 'document.cookie.csrftoken'
 async function post(data) {
     data.map(
     item =>
         fetch("http://localhost:8000/router/", {
-            headers: { "Content-type": "application/json" },
+            headers: { "Content-type": "application/json", "X-CSRFToken": csrftoken , "csrfmiddlewaretoken": "nVApnU8XGVNcCblL1lTZkwaV9cB8s544KShByxvUscuiZ2E2M1uI4ze5Iby3gn3J"},
             method: "POST",
             body: JSON.stringify({
                 name: item.name.first + item.name.last,
                 gender: item.gender,
                 email: item.email,
-                phone: item.phone,
-                data: { csrfmiddlewaretoken: "{{ csrf_token }}"},
- 
+                phone: item.phone, 
             }),
             
         })
--- hello_DRF/router/views.py
+++ hello_DRF/router/views.py
@@ -4,22 +4,26 @@ from django.http import HttpResponse
 from django.utils.translation import check_for_language
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import serializers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import Employee
 from .serializers import EmpSerializer
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 # Create your views here.
 
+
 @api_view(['GET', 'POST'])
 def create_records(request):
-    print(request.headers)
+    print(request.__dict__)
+    print(request._full_data)
     if request.method == "POST":
         print(request.data)
         serializer = EmpSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(request.data)
+            return Response(request.data)
 
     if request.method == "GET":
         return render(request, './router/index.html')
